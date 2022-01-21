0c1228d1b958869c82a6cbc066c88f72e820a80f
0webdeveloper <0webdeveloper@gmail.com>
2022-01-21 09:07:53 +0300

    Post-list transition-group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,12 +2,15 @@
     <div>
       <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <post-item
-          v-for="post in posts"
-          :post="post"
-          :key="post.id"
-          @removeTodo="$emit('remTodo', post.id)"
-        />
+        <transition-group name='post-list'>
+          <post-item
+            v-for="post in posts"
+            :post="post"
+            :key="post.id"
+            @removeTodo="$emit('remTodo', post.id)"
+          />
+        </transition-group>
+
     </div>
       <h3 v-else>Список постов пуст</h3>
     </div>
@@ -33,5 +36,19 @@ export default {
 <style scoped>
   h3 {
     margin: 15px auto;
+  }
+
+  .post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .post-list-enter-active,
+  .post-list-leave-active {
+    transition: all 1s ease;
+  }
+  .post-list-enter-from,
+  .post-list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
   }
 </style>
