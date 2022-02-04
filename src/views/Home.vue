afedfe9b75db3fb2cfdc9d178faca0dd228fb385
0webdeveloper <0webdeveloper@gmail.com>
2022-02-04 11:48:08 +0300

    create new macBranch for testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -146,11 +146,7 @@ export default {
           }
         })
         this.totalPage = Math.ceil( responce.headers['x-total-count'] / this.Limit)
-<<<<<<< HEAD
         this.posts = [...this.posts, ...responce.data]
-=======
-        this.posts = [...this.posts, ...resonce.data]
->>>>>>> 9d384aa154f05ca5a32cc05bbd120af31e98f54f
       } catch (e) {
         alert('error')
       }
@@ -166,17 +162,12 @@ export default {
       threshold: 1.0
     }
     const callback = (entries, observer) => {
-<<<<<<< HEAD
+
       entries.forEach(entry => {
           if (entry.isIntersecting && this.page < this.totalPage) {
             this.loadMorePosts();
           }
       });
-=======
-      if (entries[0].isIntersecting) {
-        this.loadMorePosts();
-      }
->>>>>>> 9d384aa154f05ca5a32cc05bbd120af31e98f54f
     };
     const observer = new IntersectionObserver(callback, options);
     observer.observe(this.$refs.observer);
@@ -244,9 +235,5 @@ PostList
 }
 .observer {
   height: 30px;
-<<<<<<< HEAD
-=======
-  background: green;
->>>>>>> 9d384aa154f05ca5a32cc05bbd120af31e98f54f
 }
 </style>
